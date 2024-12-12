a80b0f914de48b85e1fea1674909a10f0eb936fc
Simon Hornik <simon.hornik@student.gyarab.cz>
2024-12-12 09:38:04 +0100

    python

--- test.py
+++ test.py
@@ -1,13 +1,13 @@
-def fibonnaci(n):
+def fibonacci(n):
     if isinstance(n, int) and n >= 0:
         if n == 0:
             return 0
         elif n == 1:
             return 1
         else:
-            return fibonnaci(n-1) + fibonnaci(n-2)
+            return fibonacci(n-1) + fibonacci(n-2)
     else:
         raise ValueError("n must be a non-negative integer")
-    
+n = 10
 if __name__ == "__main__":
-    print(fibonnaci(1))
+    print(fibonacci(n))
