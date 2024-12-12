e78e47ec7f359a702fdc010c02e96ed347ff2360
Simon Hornik <simon.hornik@student.gyarab.cz>
2024-12-12 09:58:25 +0100

    is_prime

--- test.py
+++ test.py
@@ -1,13 +1,20 @@
-def fibonacci(n):
-    if isinstance(n, int) and n >= 0:
-        if n == 0:
-            return 0
-        elif n == 1:
-            return 1
-        else:
-            return fibonacci(n-1) + fibonacci(n-2)
-    else:
-        raise ValueError("n must be a non-negative integer")
-n = 10
-if __name__ == "__main__":
-    print(fibonacci(n))
+def is_prime(number):
+
+    if not isinstance(number, int) or number <= 0:
+        raise ValueError("Zadejte realne cislo vetsi nez 0")
+
+    if number == 1:
+        return False 
+    for i in range(2, int(number ** 0.5) + 1):
+        if number % i == 0:
+            return False
+
+    return True
+
+# Příklad použití:
+try:
+    print(is_prime(7))  
+    print(is_prime(10))  
+    print(is_prime(-5))  
+except ValueError as e:
+    print(e)
