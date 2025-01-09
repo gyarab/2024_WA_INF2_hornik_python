51cb62f514c54b496110e53c2ac4a1feadf97ef0
Simon Hornik <simon.hornik@student.gyarab.cz>
2025-01-09 09:01:58 +0100

    commit

--- test.py
+++ test.py
@@ -1,11 +1,4 @@
-def primes_in_range(a, b):
-    if not isinstance(a, int) or not isinstance(b, int):
-        raise ValueError("Error")
-    if a > b:
-        a, b = b, a
-
-
-    def is_prime(n):
+def is_prime(n):
         if n < 2:
             return False
         for i in range(2, int(n**0.5) + 1):
@@ -13,9 +6,15 @@ def primes_in_range(a, b):
                 return False
         return True
 
+def primes_in_range(a, b):
+    if not isinstance(a, int) or not isinstance(b, int):
+        raise ValueError("Error")
+    if a > b:
+        a, b = b, a
+
     return [x for x in range(a, b + 1) if is_prime(x)]
 
 
 if __name__ == "__main__":
-    primes = primes_in_range(12, 10)
+    primes = primes_in_range(2, 10)
     print(primes)
