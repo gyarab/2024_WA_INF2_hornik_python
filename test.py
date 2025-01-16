27453992afe58d7104ddd020069efa04a6f3a8a4
Simon Hornik <simon.hornik@student.gyarab.cz>
2025-01-16 09:36:55 +0100

    commit

--- test.py
+++ test.py
@@ -1,41 +1,47 @@
-def caesar_encode(text, shift=3):
-    encoded_text = ""
-    for char in text:
-        if char.isalpha() and char.isascii():  
-            start = 65 if char.isupper() else 97
-            encoded_char = chr(((ord(char) - start + shift) % 26) + start)
-            encoded_text += encoded_char
-        elif not char.isalnum():
-            encoded_text += " "
-        else:
-            raise ValueError("Používej jen PÍSMENA z anglické abecedy.")
-    return encoded_text
+def morse(text):
+    morse_dict = {
+        'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.',
+        'F': '..-.', 'G': '--.', 'H': '....', 'I': '..', 'J': '.---',
+        'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---',
+        'P': '.--.', 'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-',
+        'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-', 'Y': '-.--',
+        'Z': '--..', '0': '-----', '1': '.----', '2': '..---',
+        '3': '...--', '4': '....-', '5': '.....', '6': '-....',
+        '7': '--...', '8': '---..', '9': '----.',
+        '.': '.-.-.-', ',': '--..--', '?': '..--..', '!': '-.-.--',
+        ':': '---...', ';': '-.-.-.', "'": '.----.', '"': '.-..-.',
+        '/': '-..-.', '(': '-.--.', ')': '-.--.-', '&': '.-...',
+        ':': '---...', '=': '-...-', '+': '.-.-.', '-': '-....-',
+        '_': '..--.-', '@': '.--.-.', ' ': '/',  
+    }
 
-def caesar_decode(text, shift=3):
-    decoded_text = ""
-    for char in text:
-        if char.isalpha() and char.isascii():  
-            start = 65 if char.isupper() else 97
-            decoded_char = chr(((ord(char) - start - shift) % 26) + start)
-            decoded_text += decoded_char
-        elif not char.isalnum():
-            decoded_text += " "
+    def remove_diacritics(text):
+        diacritics_map = {
+            'á': 'a', 'č': 'c', 'ď': 'd', 'é': 'e', 'ě': 'e', 'í': 'i',
+            'ň': 'n', 'ó': 'o', 'ř': 'r', 'š': 's', 'ť': 't', 'ú': 'u',
+            'ů': 'u', 'ý': 'y', 'ž': 'z',
+        }
+        normalized_text = ''
+        for char in text:
+            if char in diacritics_map:
+                normalized_text += diacritics_map[char]
+            else:
+                normalized_text += char
+        return normalized_text
+    
+    normalized_text = remove_diacritics(text)
+
+    morse_code = []
+    for char in normalized_text.upper():
+        if char in morse_dict:
+            morse_code.append(morse_dict[char])
         else:
-            raise ValueError("Používej jen PÍSMENA z anglické abecedy.")
-    return decoded_text
+            continue
+
+    return ' '.join(morse_code)
 
 if __name__ == "__main__":
-    text = "Hello"
-    encoded = caesar_encode(text)
-    print(encoded)
-    text = "Kh oor"
-    decoded = caesar_decode(text)
-    print(decoded)
-    text = "Arabska"
-    encoded = caesar_encode(text)
-    print(encoded)
-    text = "Yixyixyix"
-    encoded = caesar_encode(text)
-    print(encoded)
-
+    print(morse("Hello World"))
+    print(morse("Ahoj světe!"))
+    
 
