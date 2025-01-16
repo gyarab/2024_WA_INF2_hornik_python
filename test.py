3b837b81d3841b868edb51a228695ce6dbbeb5b5
Simon Hornik <simon.hornik@student.gyarab.cz>
2025-01-16 08:43:03 +0100

    commit

--- test.py
+++ test.py
@@ -4,9 +4,9 @@ def caesar_encode(text, shift=3):
         if char.isalpha() and char.isascii():  
             start = 65 if char.isupper() else 97
             encoded_char = chr(((ord(char) - start + shift) % 26) + start)
-            encoded_text += encoded_char
+            
         else:
-            raise ValueError("Používej jen písmena z anglické abecedy.")
+            raise ValueError("Používej jen PÍSMENA z anglické abecedy.")
     return encoded_text
 
 def caesar_decode(text, shift=3):
@@ -15,9 +15,9 @@ def caesar_decode(text, shift=3):
         if char.isalpha() and char.isascii():  
             start = 65 if char.isupper() else 97
             decoded_char = chr(((ord(char) - start - shift) % 26) + start)
-            decoded_text += decoded_char
+            
         else:
-            raise ValueError("Používej jen písmena z anglické abecedy.")
+            raise ValueError("Používej jen PÍSMENA z anglické abecedy.")
     return decoded_text
 
 if __name__ == "__main__":
