af0104bdcc595d0acc5dbb6e5c9b5126b0e17c49
Simon Hornik <simon.hornik@student.gyarab.cz>
2025-01-16 08:38:51 +0100

    commit

--- test.py
+++ test.py
@@ -1,20 +1,35 @@
-def split_into_threes(text):
-    if not isinstance(text, str):
-        raise ValueError("Input musí být string! ")
+def caesar_encode(text, shift=3):
+    encoded_text = ""
+    for char in text:
+        if char.isalpha() and char.isascii():  
+            start = 65 if char.isupper() else 97
+            encoded_char = chr(((ord(char) - start + shift) % 26) + start)
+            encoded_text += encoded_char
+        else:
+            raise ValueError("Používej jen písmena z anglické abecedy.")
+    return encoded_text
 
-    length = len(text)
-    part_size = length // 3
-    remainder = length % 3
-
-    parts = [text[i:i+part_size] for i in range(0, length-remainder, part_size)]
-    if remainder:
-        parts.append(text[-remainder:])
-
-    return parts
-
+def caesar_decode(text, shift=3):
+    decoded_text = ""
+    for char in text:
+        if char.isalpha() and char.isascii():  
+            start = 65 if char.isupper() else 97
+            decoded_char = chr(((ord(char) - start - shift) % 26) + start)
+            decoded_text += decoded_char
+        else:
+            raise ValueError("Používej jen písmena z anglické abecedy.")
+    return decoded_text
 
 if __name__ == "__main__":
-    chain = split_into_threes("123456789")
-    print(chain)
+    text = "Hello"
+    encoded = caesar_encode(text)
+    print(encoded)
+    text = "Khoor"
+    decoded = caesar_decode(text)
+    print(decoded)
+    text = "Arabská"
+    encoded = caesar_encode(text)
+    print(encoded)
+
 
 
