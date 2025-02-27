9f48e89268043eee4034744467e1121720c940b4
Simon Hornik <simon.hornik@student.gyarab.cz>
2025-02-27 10:02:26 +0100

    fix

--- cms/urls.py
+++ cms/urls.py
@@ -16,10 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from content.views import homepage, hello
+from content.views import homepage
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', homepage),
-    path('hello', hello),
 ]
--- content/views.py
+++ content/views.py
@@ -1,7 +1,6 @@
+from django.shortcuts import render
 
 def homepage(request):
-    
-
 
     return render(request, 'content/homepage.html')
     
