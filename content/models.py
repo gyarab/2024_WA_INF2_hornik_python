b72efee949dcfe46e5d776936ff5fe909307eab9
Simon Hornik <simon.hornik@student.gyarab.cz>
2025-03-02 20:39:29 +0100

    cmt

--- content/models.py
+++ content/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Band(models.Model):
+    name = models.CharField(max_length=100)
+    genre = models.CharField(max_length=50)
+    formed_year = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+class Album(models.Model):
+    title = models.CharField(max_length=100)
+    release_year = models.IntegerField()
+    band = models.ForeignKey(Band, on_delete=models.CASCADE, related_name="albums")
+
+    def __str__(self):
+        return self.title
--- content/views.py
+++ content/views.py
@@ -1,15 +1,65 @@
-from django.http import HttpResponse
+from rest_framework import generics
+from django.http import JsonResponse
+from .models import Band, Album
+
 
 def homepage(request):
-    ret = '''
-    <!DOCTYPE html>
-    <html>
-    <head></head>
-    <body>
-    <h1>Homepage</h1>
-    </body></html>
-    '''
-    return HttpResponse(ret)
+    return JsonResponse({
+        "message": "Welcome to the Music API. You can view bands and albums by visiting /bands/ and /albums/."
+    })
 
-def hello(request):
-    return HttpResponse('Ahoj!')
+
+class BandListCreate(generics.ListCreateAPIView):
+    queryset = Band.objects.all()
+    serializer_class = None
+
+    def get_serializer(self, *args, **kwargs):
+        from rest_framework import serializers
+        class BandSerializer(serializers.ModelSerializer):
+            class Meta:
+                model = Band
+                fields = '__all__'
+        self.serializer_class = BandSerializer
+        return super().get_serializer(*args, **kwargs)
+
+
+class BandDetail(generics.RetrieveAPIView):
+    queryset = Band.objects.all()
+    serializer_class = None
+
+    def get_serializer(self, *args, **kwargs):
+        from rest_framework import serializers
+        class BandSerializer(serializers.ModelSerializer):
+            class Meta:
+                model = Band
+                fields = '__all__'
+        self.serializer_class = BandSerializer
+        return super().get_serializer(*args, **kwargs)
+
+
+class AlbumListCreate(generics.ListCreateAPIView):
+    queryset = Album.objects.all()
+    serializer_class = None
+
+    def get_serializer(self, *args, **kwargs):
+        from rest_framework import serializers
+        class AlbumSerializer(serializers.ModelSerializer):
+            class Meta:
+                model = Album
+                fields = '__all__'
+        self.serializer_class = AlbumSerializer
+        return super().get_serializer(*args, **kwargs)
+
+
+class AlbumDetail(generics.RetrieveAPIView):
+    queryset = Album.objects.all()
+    serializer_class = None
+
+    def get_serializer(self, *args, **kwargs):
+        from rest_framework import serializers
+        class AlbumSerializer(serializers.ModelSerializer):
+            class Meta:
+                model = Album
+                fields = '__all__'
+        self.serializer_class = AlbumSerializer
+        return super().get_serializer(*args, **kwargs)
