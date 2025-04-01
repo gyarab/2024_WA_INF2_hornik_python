73b50fedb2a8f9c6e98124ac8f8d588b0457a1d9
Simon Hornik <simon.hornik@student.gyarab.cz>
2025-04-01 18:16:49 +0200

    pocem kamdes

--- content/models.py
+++ content/models.py
@@ -1,17 +1,26 @@
 from django.db import models
 
 class Band(models.Model):
-    name = models.CharField(max_length=100)
-    genre = models.CharField(max_length=50)
+    name = models.CharField(max_length=255)
+    genre = models.CharField(max_length=100)
     formed_year = models.IntegerField()
 
     def __str__(self):
         return self.name
 
 class Album(models.Model):
-    title = models.CharField(max_length=100)
+    band = models.ForeignKey(Band, on_delete=models.CASCADE, related_name="albums", null=True)
+    title = models.CharField(max_length=255, null=True)
     release_year = models.IntegerField()
-    band = models.ForeignKey(Band, on_delete=models.CASCADE, related_name="albums")
+    cover = models.URLField(null=True, blank=True)  # URL to the album cover image
+    
+    def __str__(self):
+        return f"{self.title} - {self.band.name}"
+
+class Track(models.Model):
+    album = models.ForeignKey(Album, on_delete=models.CASCADE, related_name="tracks")
+    title = models.CharField(max_length=255, null=True)
+    duration = models.TimeField()
 
     def __str__(self):
         return self.title
--- content/views.py
+++ content/views.py
@@ -1,15 +1,19 @@
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Band, Album, Track
 
 def homepage(request):
-    ret = '''
-    <!DOCTYPE html>
-    <html>
-    <head></head>
-    <body>
-    <h1>Homepage</h1>
-    </body></html>
-    '''
-    return HttpResponse(ret)
+    return render(request, "content/homepage.html")
 
-def hello(request):
-    return HttpResponse('Ahoj!')
+def band_list(request):
+    bands = Band.objects.all()
+    return render(request, "content/band_list.html", {"bands": bands})
+
+def album_list(request, band_id):
+    band = get_object_or_404(Band, id=band_id)
+    albums = Album.objects.filter(band=band)
+    return render(request, "content/album_list.html", {"band": band, "albums": albums})
+
+def track_list(request, album_id):
+    album = get_object_or_404(Album, id=album_id)
+    tracks = Track.objects.filter(album=album)
+    return render(request, "content/track_list.html", {"album": album, "tracks": tracks})
