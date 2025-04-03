82780812815362a7ab4593e9be891899bda765a8
Simon Hornik <simon.hornik@student.gyarab.cz>
2025-04-03 11:49:27 +0200

    bruno je goat

--- content/models.py
+++ content/models.py
@@ -4,18 +4,24 @@ from django.db import models
 class Band(models.Model):
     name = models.CharField(max_length=100)
     founded = models.IntegerField(null=True, blank=True)
-    genre = models.CharField(max_length=100, null=True, blank=True)
+    genre = models.ManyToManyField('Genre', related_name="bands", blank=True)
     description = models.TextField()
-    logo = models.URLField(max_length=500, blank=True, null=True)    
+    logo = models.URLField(max_length=500, blank=True, null=True)
     
     def __str__(self):
         return self.name
 
 class Album(models.Model):
-    band = models.ForeignKey(Band, related_name='albums', on_delete=models.CASCADE, null=True)
+    band = models.ForeignKey(Band, related_name='album', on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     release_date = models.IntegerField(null=True, blank=True)
     cover_image = models.URLField(max_length=500, blank=True, null=True)
     
     def __str__(self):
         return self.title
+
+class Genre(models.Model):
+    name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
--- content/views.py
+++ content/views.py
@@ -8,5 +8,5 @@ def homepage(request):
 
 def band_detail(request, band_id):
     band = get_object_or_404(Band, id=band_id)  # Najdeme kapelu podle ID
-    albums = band.albums.all()  # Načteme všechna alba této kapely
+    albums = band.album.all()  # Načteme všechna alba této kapely
     return render(request, 'content/band_detail.html', {'band': band, 'albums': albums})
