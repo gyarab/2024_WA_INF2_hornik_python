a213a5a3f20230d975128916a22c508d0c29983c
Simon Hornik <simon.hornik@student.gyarab.cz>
2025-05-15 21:24:20 +0200

    filtry a css uprava

--- content/admin.py
+++ content/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Band, Album
+from .models import Band, Album, Genre
 
 admin.site.register(Band)
 admin.site.register(Album)
+admin.site.register(Genre)
--- content/views.py
+++ content/views.py
@@ -1,12 +1,29 @@
 # content/views.py
 from django.shortcuts import render, get_object_or_404
-from .models import Band
+from django.db.models import Q
+from .models import Band, Genre
 
 def homepage(request):
-    bands = Band.objects.all()  # Načteme všechny kapely
-    return render(request, 'content/homepage.html', {'bands': bands})
+    genre_id = request.GET.get('genre')
+    search_query = request.GET.get('search')
+    
+    bands = Band.objects.all()
+
+    if genre_id:
+        bands = bands.filter(genre__id=genre_id)
+    
+    if search_query:
+        bands = bands.filter(name__icontains=search_query)
+
+    genres = Genre.objects.all()
+    
+    return render(request, 'content/homepage.html', {
+        'bands': bands,
+        'genres': genres,
+        'selected_genre': int(genre_id) if genre_id else None
+    })
 
 def band_detail(request, band_id):
-    band = get_object_or_404(Band, id=band_id)  # Najdeme kapelu podle ID
-    albums = band.album.all()  # Načteme všechna alba této kapely
+    band = get_object_or_404(Band, id=band_id)
+    albums = band.album.all()
     return render(request, 'content/band_detail.html', {'band': band, 'albums': albums})
